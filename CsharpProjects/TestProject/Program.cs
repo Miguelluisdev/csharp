82cd1e5a806279e83814283d36a7b855968efa8e
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-11-12 19:55:45 -0300

    feat: aprendendo c#

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs 
+{
+    internal class Program 
+    {
+        struct Dadosdecadastro 
+        {
+            
+         public string Nome;
+         public string SobreNome;
+        public UInt32 Idade;
+        }
+    static void Main(string[] args)
+    {
+        Dadosdecadastro  meuCadatro;
+        meuCadatro.Nome = "darius";
+        meuCadatro.SobreNome = "lannister";
+        meuCadatro.Idade = 19;
+    }
+        
+    }
+}
