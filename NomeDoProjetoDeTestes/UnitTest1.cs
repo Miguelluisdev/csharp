14480383536de165f11d89fb2084602afd455a20
Miguel Luis <miguelluisataideferreira68@gmail.com>
2025-01-21 19:39:27 -0300

    feat: testando comandos do c#

---
+++ NomeDoProjetoDeTestes/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace NomeDoProjetoDeTestes;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+// A skeleton of a C# program
+using System;
+
+// Your program starts here:
+Console.WriteLine("Hello world!");
+
+namespace YourNamespace
+{
+    class YourClass
+    {
+        Console.WriteLine("Tipos de dados em C# string , bool , int , long , char , decimal  , DateTime , Float ")
+    }
+
+    struct YourStruct
+    {
+    }
+
+    interface IYourInterface
+    {
+    }
+
+    delegate int YourDelegate();
+
+    enum YourEnum
+    {
+    }
+
+    namespace YourNestedNamespace
+    {
+        struct YourStruct
+        {
+            
+        }
+    }
+}
